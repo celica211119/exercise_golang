a1d67e8651dcd30c65056a6f5b4365d3b27b2937
celica <celicachen170701@gmail.com>
2025-01-31 14:46:49 +0800

    测试

--- goExample/24.03.25/76.HTTP Client/http-clients.go
+++ goExample/24.03.25/76.HTTP Client/http-clients.go
@@ -4,22 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
+
+	_ "github.com/apache/skywalking-go"
 )
 
 func main() {
-	resp, err := http.Get("https://gobyexample.com")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	for {
+		resp, err := http.Get("http://127.0.0.1:19091/owners/1882131")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
 
-	fmt.Println("Response status:", resp.Status)
+		fmt.Println("Response status:", resp.Status)
 
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
+		scanner := bufio.NewScanner(resp.Body)
+		for i := 0; scanner.Scan() && i < 5; i++ {
+			fmt.Println(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
 	}
 }
